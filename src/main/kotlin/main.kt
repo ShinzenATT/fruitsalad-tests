b38cca50cdc0513dcb9959745d1803d3b6ad437d
shinzenatt <alen.mukaca@gmail.com>
2022-05-21 14:52:31 +0200

    Implemented the solution for finding cheapest stand

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,26 @@
+import fruitObj.*
+
+val stands: List<FruitStand> = listOf(
+    FruitStand("Gbg Stand",
+        setOf(
+            Product("Cherry Basket", 5),
+            Product("Peach Basket", 3)
+        )
+    ),
+    FruitStand("Partille Stand",
+        setOf(
+            Product("Cherry Basket", 9),
+            Product("Peach Basket", 2)
+        )
+    ),
+    FruitStand("Kotlin Stand",
+        setOf(
+            Product("Cherry Basket", 6),
+            Product("Peach Basket", 1)
+        )
+    )
+)
+
 fun main() {
-    println("Hello, world!")
+    println( stands.minByOrNull { it.totalCost } ?: "No fruit stands available" )
 }
