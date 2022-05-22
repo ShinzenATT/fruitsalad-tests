a19940c2253d72ad050639a1218bc68f19524447
shinzenatt <alen.mukaca@gmail.com>
2022-05-22 13:48:47 +0200

    Moved  BasketEntry to a new file

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -35,15 +35,7 @@ val stands: List<FruitStand> = listOf(
     )
 )
 
-/**
- * A data class for containing purchased baskets while keeping track of which stands they were purchased from
- * @param basket Purchased Basket
- * @param srcStand The stand the basket was purchased from
- */
-private data class BasketEntry(val basket: Basket, val srcStand: FruitStand)
-{
-    override fun  toString(): String = "Basket that contains ${basket.contents.map { it.fruit.name }} with cost of ${basket.price}kr  from ${srcStand.name}"
-}
+
 
 fun main() {
     val purchases = listOf(
